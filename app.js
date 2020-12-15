b0c65bd9e830c701a0687aeab7d5c956170fab51
grgsferry <grgsferry@gmail.com>
2020-12-16 01:32:19 +0700

    Add select and button functions

--- app.js
+++ app.js
@@ -5,44 +5,68 @@ let clearAllButton = document.getElementById('clearallbutton');
 let deleteButton = document.getElementById('deletebutton');
 let moveUpButton = document.getElementById('moveupbutton');
 let moveDownButton = document.getElementById('movedownbutton');
-let tableRowsCount = 1;
 
 submitButton.addEventListener('click', () => {
     let todoItem = todoInput.value;
     let template = `
         <tr>
             <td>${todoItem}</td>
-            <td><input type="checkbox"></input></td>
+            <td><input type="checkbox"></input></td> 
         </tr>
     `;
-    table.innerHTML += template;
+    table.getElementsByTagName('tbody')[0].innerHTML += template;
     todoInput.value = '';
-    table.style.display = "block";
-    deleteButton.style.display = "inline-block";
-    moveUpButton.style.display = "inline-block";
-    moveDownButton.style.display = "inline-block";
-    tableRowsCount++;
+    // table.style.display = 'inline';
+    // deleteButton.style.display = 'inline';
+    // moveUpButton.style.display = 'inline';
+    // moveDownButton.style.display = 'inline';
+    getSelectedRow();
 });
 
 clearAllButton.addEventListener('click', () => {
-    table.innerHTML = `
-    <tr>
-        <th>To-Do List</th>
-        <th>Status</th>
-    </tr>
-    `;
+    table.getElementsByTagName('tbody')[0].innerHTML = '';
     todoInput.value = '';
-    table.style.display = "none";
-    deleteButton.style.display = "none";
-    moveUpButton.style.display = "none";
-    moveDownButton.style.display = "none";
-    tableRowsCount = 1;
+    // table.style.display = 'none';
+    // deleteButton.style.display = 'none';
+    // moveUpButton.style.display = 'none';
+    // moveDownButton.style.display = 'none';
+    index = undefined;
 });
 
-// for(i = 1; i < tableRowsCount; i++) {
+let index;
+function getSelectedRow() {
+    for(i = 1; i < table.rows.length; i++) {
+        table.rows[i].onclick = function() {
+            if(typeof index !== 'undefined') {
+                table.rows[index].classList.toggle('selected');
+            }; //this body code will un-toggles the class of the previous index (the body code returns false)
+            index = this.rowIndex; //this refers to table.rows[i]
+            this.classList.toggle('selected'); //this will re-toggles the class to the new index
+            //(this onclick function code always returns true because of the toggle method above)
+        }
+    }
+};
 
-// }
+deleteButton.addEventListener('click', function(){
+    table.deleteRow(index);
+    getSelectedRow; //this will re-calibrate the table.rows[i]s
+    index = undefined; //this deletes the index to be freshly started
+});
 
-// table.rows[0].addEventListener('click', function() {
-//     alert('ey');
-// });
+moveUpButton.addEventListener('click', function(){
+    let rows = table.rows;
+    let parent = rows[index].parentNode;
+    if(index > 1) {
+        parent.insertBefore(rows[index], rows[index - 1]);
+        index--;
+    }
+});
+
+moveDownButton.addEventListener('click', function(){
+    let rows = table.rows;
+    let parent = rows[index].parentNode;
+    if(index < rows.length - 1) {
+        parent.insertBefore(rows[index + 1], rows[index]);
+        index++;
+    }
+});
